91a15f83ec9f3dbef9ee6b59216bb2f71fa7a389
Artem Lapenkov <artbreenche@gmail.com>
2022-09-13 20:53:21 +0300

    first change

---
+++ index.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.green');
+const btn2 = document.querySelector('.red');
+const btn3 = document.querySelector('.yellow');
+
+btn.onclick = function() {
+ 
+  document.body.style.backgroundColor = btn.value;
+
+}
+
+btn2.onclick = function() {
+ 
+  document.body.style.backgroundColor = btn2.value;
+
+}
+
+btn3.onclick = function() {
+ 
+  document.body.style.backgroundColor = btn3.value;
+
+}
